e1058b2dbbf1757c1b40d002a3e5b147df6d2fce
Lacy <lacy.simpson@usm.edu>
2018-09-16 02:09:22 -0500

    printing the map to make sure map values are storing correctly

--- project1/source/main.py
+++ project1/source/main.py
@@ -9,12 +9,13 @@ def main():
     displayMaze(theMaze)
     print(findEntryPoint(theMaze))
 
-    map = createMap()
-    print(map)
+    map = createMap(theMaze)
 
     # prints the value stored at the key '02'
     # use this syntax to check or change values at map key locations
-    print(map['02'])
+    for row in range(10):
+        n = 10 * row
+        print(map[(n)], map[n+1], map[n+2], map[n+3], map[n+4], map[n+5], map[n+6], map[n+7], map[n+8], map[n+9])
 
     entryRow, entryColumn = findEntryPoint(theMaze)
     print(entryRow, entryColumn)
