2f8e33d1d4c60136a482cab5b39f8e1b8d89ced7
Lacy <lacy.simpson@usm.edu>
2018-09-17 15:19:10 -0500

    Added a function to the node class that will append the neighbors to the neighbors list. This function will look at the left, up, right, and down locations and ensure that the neighbors are valid and on the map.

--- project1/source/newNode.py
+++ project1/source/newNode.py
@@ -1,10 +1,11 @@
+from source.breadthFirstSearch.py import *
 class Node:
     def __init__(self):
         self.location = 0
         self.data = ''
         self.fringe = []
         self.visited = False
-        self.previous = []
+        self.path = []
 
     def getLocation(self, point):
         self.location = point
@@ -12,7 +13,7 @@ class Node:
     def getData(self, theMap, n):
         self.data = theMap[n][1]
 
-    def getFringe(self, Node):
+    def getNeighbors(self, Node):
         if Node not in self.fringe:
             self.fringe.append(Node)
 
@@ -22,3 +23,26 @@ class Node:
                 return True
             else:
                 return False
+    def setPath(self, previousNode):
+        if previousNode == None:
+            self.path.append(self.location)
+        else:
+            self.path.append(previousNode.path)
+
+    def findValidNeighbors(self):
+        if ((self.location != 0) and (self.location % 10 != 0) and (self.data == 'P' or self.data == 'X')):
+            leftNeighbor = self.location - 1
+            self.getNeighbors(leftNeighbor)
+
+        if (not(self.location < 0)) and (self.data == 'P' or self.data == 'X'):
+            topNeighbor = self.location - 10
+            self.getNeighbors(topNeighbor)
+
+        remainder = (self.location + 1) % 10
+        if (remainder != 0) and (self.data == 'P' or self.data == 'X'):
+            rightNeighbor = self.location + 1
+            self.getNeighbors(rightNeighbor)
+
+        if (not (self.location + 10) > 99) and (self.data == 'P' or self.data == 'X'):
+            bottomNeighbor = self.location + 10
+            self.getNeighbors(bottomNeighbor)
