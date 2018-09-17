2339d931097dc882fc20229b3f8ceb2f8befdf53
Lacy <lacy.simpson@usm.edu>
2018-09-17 00:31:50 -0500

    minor changes made to test and implement new methods.

--- project1/source/main.py
+++ project1/source/main.py
@@ -1,5 +1,5 @@
 from source.mazeRunner import *
-from source.newNode import newNode
+from source.newNode import Node
 
 
 def main():
@@ -7,21 +7,22 @@ def main():
 
     theMaze = createMaze(readInFile(file))
     displayMaze(theMaze)
-    print(findEntryPoint(theMaze))
+
 
     map = createMap(theMaze)
-
-    # prints the value stored at the key '02'
+    print(findEntryPoint(map))
+    # prints the values stored at each key "location"
     # use this syntax to check or change values at map key locations
     for row in range(10):
         n = 10 * row
         print(map[(n)], map[n+1], map[n+2], map[n+3], map[n+4], map[n+5], map[n+6], map[n+7], map[n+8], map[n+9])
 
-    entryRow, entryColumn = findEntryPoint(theMaze)
-    print(entryRow, entryColumn)
-    initialPosition = newNode(entryRow, entryColumn)
-    initialPosition.data = theMaze[entryRow][entryColumn]
+    entryPoint = findEntryPoint(map)
+    initialPosition = Node()
+    initialPosition.getLocation(entryPoint)
     print(initialPosition.location)
+    initialPosition.getData(map, n)
+    print(initialPosition.data)
 
 
 main()
