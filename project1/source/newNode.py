9d84930137491036ee35f256185cf492e9d3ef5d
Lacy <lacy.simpson@usm.edu>
2018-09-17 00:33:53 -0500

    changed the contents of the Node class, and added get methods.

--- project1/source/newNode.py
+++ project1/source/newNode.py
@@ -1,4 +1,24 @@
-class newNode:
-    def __init__(self, row, column):
-        self.location = {(10 * row) + column}
+class Node:
+    def __init__(self):
+        self.location = 0
+        self.data = ''
         self.fringe = []
+        self.visited = False
+        self.previous = []
+
+    def getLocation(self, point):
+        self.location = point
+
+    def getData(self, theMap, n):
+        self.data = theMap[n][1]
+
+    def getFringe(self, Node):
+        if Node not in self.fringe:
+            self.fringe.append(Node)
+
+    def isVisited(self, visited):
+        for number in visited:
+            if self.location == number:
+                return True
+            else:
+                return False
