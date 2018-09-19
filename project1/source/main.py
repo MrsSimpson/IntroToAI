ad7f3893fcbb049411d63c70e5cb59150f7c6b67
Lacy <lacy.simpson@usm.edu>
2018-09-18 23:28:58 -0500

    Updated the calls to the Node class for testing. The node class now successfully sets all values as intended with no errors as of yet. Changes will be made when the breadth first search and deapth first search logic comes in to play.

--- project1/source/main.py
+++ project1/source/main.py
@@ -1,5 +1,6 @@
-from source.mazeRunner import *
+
 from source.newNode import *
+from source.mazeRunner import *
 
 
 def main():
@@ -17,12 +18,19 @@ def main():
         n = 10 * row
         print(map[(n)], map[n+1], map[n+2], map[n+3], map[n+4], map[n+5], map[n+6], map[n+7], map[n+8], map[n+9])
 
-    entryPoint = findEntryPoint(map)
-    initialPosition = Node()
-    initialPosition.getLocation(entryPoint)
+    prevVisited = [60]
+    point = findEntryPoint(map)
+    initialPosition = Node(point, map)
+    #initialPosition.setNewNode(point, map)
+    data = map[point][1]
+    initialPosition.findValidNeighbors(point, map)
+    initialPosition.setPath(initialPosition)
+    initialPosition.visited = initialPosition.isVisited(prevVisited)
     print(initialPosition.location)
-    initialPosition.getData(map, n)
     print(initialPosition.data)
+    print(initialPosition.path)
+    print(initialPosition.visited)
+    print(initialPosition.neighbors)
 
 
 main()
