7298f9f7a02528f66866da2dd6bf68dc17fdc663
Lacy <lacy.simpson@usm.edu>
2018-09-20 01:06:43 -0500

    deleted all of the print funcitons that I was using to test.

--- project1/source/main.py
+++ project1/source/main.py
@@ -9,30 +9,9 @@ def main():
     theMaze = createMaze(readInFile(file))
     displayMaze(theMaze)
 
-
     map = createMap(theMaze)
-    # prints the values stored at each key "location"
-    # use this syntax to check or change values at map key locations
-    for row in range(10):
-        n = 10 * row
-        print(map[(n)], map[n+1], map[n+2], map[n+3], map[n+4], map[n+5], map[n+6], map[n+7], map[n+8], map[n+9])
 
-
-    point = findEntryPoint(map)
-    initialPosition = Node(point, map)
-    initialPosition.findValidNeighbors(point, map)
-    initialPosition.setPath(initialPosition)
-    prevVisited = []
-    prevVisited.append(initialPosition.location)
-    #initialPosition.addToTheVisitedList(prevVisited)
-    initialPosition.visited = initialPosition.isVisited(prevVisited)
-    print(initialPosition.location)
-    print(initialPosition.data)
-    print(initialPosition.neighbors)
-    print(initialPosition.visited)
-    print(initialPosition.path)
-
-    beginSearch(map)
+    BFS(map)
 
 
 
