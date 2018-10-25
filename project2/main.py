3e597ca0211db326a618448d02880f547dec2c91
Lacy <lacy.simpson@usm.edu>
2018-10-25 00:09:26 -0500

    changed the visited data structure to a dictionary instead of an array. This allows for using the dictionary hashing function to search the previously visited states. The necessary initial node values are set in main. A timer is started to enable seeing the run time for the search.

--- project2/main.py
+++ project2/main.py
@@ -1,32 +1,23 @@
 """Run the slider 8 program"""
 
-
-from subpackages.solve_the_puzzle import create_starting_state
+import time
+from subpackages.breadth_first_search import *
+from subpackages.solve_the_puzzle import *
 from subpackages.node import Node
+
 
 
 def main():
     """Main will run the slider 8 puzzle program by calling necessary functions."""
     menu()
+    visited_map = {}
     initial_environment = create_starting_state()
+    start_time = time.clock()
     current_node = Node(initial_environment)
-
-
-
-def menu():
-    """Give the user a choice on how to create the puzzle."""
-    print("Please select one of the following choices.")
-    print("1. Create Random Starting State.")
-    print("2. Pick a Starting Space.")
-
-
-def pick_a_search():
-    """Give the user a choice on which search to perform."""
-    print("Please select one of the following choices to solve the slider puzzle.")
-    print("1. Solve using breadth first search")
-    print("2. Solve using depth first search")
-    print("3. Solve with A* Search using the number of misplaced tiles.")
-    print("4. Solve with A* Search using the manhattan distance")
+    current_node.empty_spot = current_node.find_empty_position()
+    current_node.create_state_string()
+    visited_map.update({current_node.state_string: 1})
+    begin_breadth_first_search(current_node, visited_map, start_time)
 
 
 main()
