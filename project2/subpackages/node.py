ef6c5ffc5bea35218c0139e4d509c27f27a1b68e
Lacy <lacy.simpson@usm.edu>
2018-10-25 00:12:19 -0500

    some node attributes removed and changed

--- project2/subpackages/node.py
+++ project2/subpackages/node.py
@@ -5,7 +5,8 @@ class Node:
     """node class used for creating new node states"""
     def __init__(self, environment):
         self.start_state = environment
-        self.empty_spot = self.find_empty_position()
+        self.state_string = ""
+        self.empty_spot = None
         self.depth = 0
         self.heuristic = 0
         self.is_visited = False
@@ -13,15 +14,17 @@ class Node:
 
     def find_empty_position(self):
         """find the empty position of the node's state"""
-        empty_position = -1
         for i in range(10):
             if self.start_state[i] == 0:
                 empty_position = i
-        return empty_position
+                return empty_position
+        return None
 
-    def set_is_visited(self):
-        """set the visited boolean to true"""
-        self.is_visited = True
+    def create_state_string(self):
+        """create string of integers found in the array for start_state"""
+        for number in self.start_state:
+            self.state_string += str(number)
+        return self.state_string
 
     def set_path(self, previous_node):
         """set the path of the open position"""
