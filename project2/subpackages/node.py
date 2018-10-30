b15500673ad65cad0799096d0e6076c397879468
Lacy <lacy.simpson@usm.edu>
2018-10-30 00:06:49 -0500

    The function to calculate the number of misplaced tiles was added. This function returns a value that serves for setting the heuristic of the node.

--- project2/subpackages/node.py
+++ project2/subpackages/node.py
@@ -1,15 +1,16 @@
 """node class used for creating new node states"""
 
 
-class Node(object):
+class Node():
     """node class used for creating new node states"""
     def __init__(self, environment):
         self.start_state = environment
         self.state_string = ""
         self.empty_spot = None
         self.depth = 0
-        self.heuristic = 0
+        self.heuristic = None
         self.path = []
+
 
     def find_empty_position(self):
         """find the empty position of the node's state"""
@@ -35,3 +36,18 @@ class Node(object):
             self.path.append(previous_node.path)
             self.path.append(str(previous_node.empty_spot) + " swapped for " + str(new_spot))
             return self.path
+
+    def calculate_misplaced_tiles(self):
+        """calculates the number of tiles that are out of position and creates a huristic from this number."""
+        heuristic = 0
+        for tile in range(9):
+            if tile == 8:
+                if self.start_state[8] != 0:
+                    heuristic += 1
+            else:
+                if self.start_state[tile] != (tile + 1):
+                    heuristic += 1
+
+        self.heuristic = heuristic
+        return self.heuristic
+
